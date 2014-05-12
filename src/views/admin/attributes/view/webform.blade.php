b7eb6510d0fd7111f5704be8078d58411dce254f
Alex Dover <me@alexdover.co.uk>
2014-05-12 22:03:39 +0100

    Tidied up display a bit.

--- src/views/admin/attributes/view/webform.blade.php
+++ src/views/admin/attributes/view/webform.blade.php
@@ -1,4 +1,6 @@
-<p>Notification email: {{ $data['notification_email'] }}</p>
+@if ($data['notification_email'] != '')
+	<p><i class="fa fa-envelope-o"></i> Notification email: {{ $data['notification_email'] }}</p>
+@endif
 
 <table class="table table-striped">
 	@foreach ($data['fields'] as $field)
