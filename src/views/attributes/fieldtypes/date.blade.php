8bcffd248083317cd41eb90aa351c6515ff0d767
Alex Dover <me@alexdover.co.uk>
2014-08-07 13:31:12 +0100

    Added a date class to the input group, so users can enable a date picker.

--- src/views/attributes/fieldtypes/date.blade.php
+++ src/views/attributes/fieldtypes/date.blade.php
@@ -1,9 +1,10 @@
 <div class="form-group @if (isset($invalid_fields['field_' . $field->id])) has-error @endif">
 	<label class="control-label" for="field_{{ $field->id }}">{{ $field->label }} @if ($field->required) * @endif</label>
-	<div class="input-group">
+
+	<div class="input-group date">
 		<input type="text" class="form-control" id="field_{{ $field->id }}" name="field_{{ $field->id }}" value="{{ Input::old('field_' . $field->id) }}">
 		<span class="input-group-addon">
-			<span class="glyphicon glyphicon-calendar"></span>
+			<i class="glyphicon glyphicon-calendar"></i>
 		</span>
 	</div>
 
