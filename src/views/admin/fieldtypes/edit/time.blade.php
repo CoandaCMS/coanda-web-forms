2c5eebcb2dc8c4f223a19c73bce726d88d620df0
Alex Dover <me@alexdover.co.uk>
2015-03-06 09:58:43 +0000

    Added an option to render the time input boxes as drop downs instead of inputs.

--- src/views/admin/fieldtypes/edit/time.blade.php
+++ src/views/admin/fieldtypes/edit/time.blade.php
@@ -1,1 +1,8 @@
-{{-- No further options --}}
+<div class="row">
+    <div class="col-md-12">
+        <div class="checkbox">
+            <input type="checkbox" id="field_{{ $field->id }}_render_as_dropdowns" name="field_{{ $field->id }}_custom[render_as_dropdowns]" value="true" {{ isset($field->typeData()['render_as_dropdowns']) && $field->typeData()['render_as_dropdowns'] ? ' checked="checked"' : '' }}">
+            <label for="field_{{ $field->id }}_render_as_dropdowns">Render as drop downs</label>
+        </div>
+    </div>
+</div>
--- src/views/attributes/fieldtypes/time.blade.php
+++ src/views/attributes/fieldtypes/time.blade.php
@@ -2,9 +2,24 @@
 	<label class="control-label" for="field_{{ $field->id }}">{{ $field->label }} @if ($field->required) * @endif</label>
 
 	<div class="time-input {{ (isset($field->typeData()['class']) ? $field->typeData()['class'] : '') }}">
-	    <input style="width: 50px; float: left;" type="text" class="form-control" id="field_{{ $field->id }}_hour" name="field_{{ $field->id }}[hour]" value="{{ isset(Input::old('field_' . $field->id)['hour']) ? Input::old('field_' . $field->id)['hour'] : '' }}">
-	    <span style="float: left; font-size: 20px; padding: 0 5px;" class="divider">:</span>
-	    <input style="width: 50px; float: left;" type="text" class="form-control" id="field_{{ $field->id }}_minute" name="field_{{ $field->id }}[minute]" value="{{ isset(Input::old('field_' . $field->id)['minute']) ? Input::old('field_' . $field->id)['minute'] : '' }}">
+
+        @if (isset($field->typeData()['render_as_dropdowns']) && $field->typeData()['render_as_dropdowns'])
+            <select style="width: 50px; float: left;" type="text" class="form-control" id="field_{{ $field->id }}_hour" name="field_{{ $field->id }}[hour]">
+                @foreach (range(0, 23) as $hour)
+                    <option value="{{ $hour }}" {{ isset(Input::old('field_' . $field->id)['hour']) ? (Input::old('field_' . $field->id)['hour'] == $hour ? ' selected="selected"' : '') : '' }}>{{ $hour }}</option>
+                @endforeach
+            </select>
+            <span style="float: left; font-size: 20px; padding: 0 5px;" class="divider">:</span>
+            <select style="width: 50px; float: left;" type="text" class="form-control" id="field_{{ $field->id }}_minute" name="field_{{ $field->id }}[minute]">
+                @foreach (range(0, 59) as $minute)
+                    <option value="{{ $minute }}" {{ isset(Input::old('field_' . $field->id)['minute']) ? (Input::old('field_' . $field->id)['minute'] == $minute ? ' selected="selected"' : '') : '' }}>{{ $minute }}</option>
+                @endforeach
+            </select>
+        @else
+            <input style="width: 50px; float: left;" type="text" class="form-control" id="field_{{ $field->id }}_hour" name="field_{{ $field->id }}[hour]" value="{{ isset(Input::old('field_' . $field->id)['hour']) ? Input::old('field_' . $field->id)['hour'] : '' }}">
+            <span style="float: left; font-size: 20px; padding: 0 5px;" class="divider">:</span>
+            <input style="width: 50px; float: left;" type="text" class="form-control" id="field_{{ $field->id }}_minute" name="field_{{ $field->id }}[minute]" value="{{ isset(Input::old('field_' . $field->id)['minute']) ? Input::old('field_' . $field->id)['minute'] : '' }}">
+        @endif
 
 	    <div class="clearfix"></div>
 	</div>
